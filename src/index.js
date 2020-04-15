da28ad18c6100a2ebb1e9f6e4573b481f95f77d8
S Singh <singhdeveloper@hotmail.com>
2020-04-15 16:57:47 -0500

    Use of Html webpack dev server

--- src/index.js
+++ src/index.js
@@ -2,14 +2,19 @@ import _ from 'lodash';
 import './style.css';
 import Icon from './icon.png';
 import Data from './data.xml';
+import print from './print';
 function component(){
     const element = document.createElement('div');
-    element.innerHTML = _.join(['Hello', 'Webpack'],' ');
+    element.innerHTML = _.join(['Hello', 'Webpack','is', 'good', 'really','good'],' ');
     element.classList.add('hello');
     const myIcon = new Image();
     myIcon.src = Icon;
     element.appendChild(myIcon);
     console.log(Data);
+    const btn = document.createElement('button');
+    btn.innerHTML = 'Click Me';
+    btn.onclick = print;
+    element.appendChild(btn);
     return element; 
 }
 document.body.appendChild(component());
